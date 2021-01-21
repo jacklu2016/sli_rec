a4d2ea9c2b2d182f86a62fee6bf827c0d6a28f6d
jack <56344136@qq.com>
2021-01-21 15:39:16 +0800

    range , iterator

--- sli_rec/iterator.py
+++ sli_rec/iterator.py
@@ -54,9 +54,9 @@ class Iterator:
                 ss = self.source.readline()
                 if ss == "":
                     break
-                self.source_buffer.append(ss.strip("\n").split("\t"))
+                self.source_buffer.append(ss.decode().strip("\n").split("\t"))
 
-            his_length = np.array([len(s[5].split("")) for s in self.source_buffer])
+            his_length = np.array([len(s[5].split("")) if len(s) >= 5 else 0 for s in self.source_buffer])
             tidx = his_length.argsort()
 
             _sbuf = [self.source_buffer[i] for i in tidx]
@@ -73,69 +73,70 @@ class Iterator:
                     ss = self.source_buffer.pop()
                 except IndexError:
                     break
+                print(ss)
+                if len(ss) > 1:
+                    uid = self.userdict[ss[1]] if ss[1] in self.userdict else 0
+                    mid = self.itemdict[ss[2]] if ss[2] in self.itemdict else 0
+                    cat = self.catedict[ss[3]] if ss[3] in self.catedict else 0
+                    timestepnow = float(ss[4])
 
-                uid = self.userdict[ss[1]] if ss[1] in self.userdict else 0
-                mid = self.itemdict[ss[2]] if ss[2] in self.itemdict else 0
-                cat = self.catedict[ss[3]] if ss[3] in self.catedict else 0
-                timestepnow = float(ss[4])
+                    tmp = []
+                    for fea in ss[5].split(""):
+                        m = self.itemdict[fea] if fea in self.itemdict else 0
+                        tmp.append(m)
+                    mid_list = tmp
+
+                    tmp1 = []
+                    for fea in ss[6].split(""):
+                        c = self.catedict[fea] if fea in self.catedict else 0
+                        tmp1.append(c)
+                    cat_list = tmp1
+
+                    tmp2 = []
+                    for fea in ss[7].split(""):
+                        tmp2.append(float(fea))
+                    time_list = tmp2
                 
-                tmp = []
-                for fea in ss[5].split(""):
-                    m = self.itemdict[fea] if fea in self.itemdict else 0
-                    tmp.append(m)
-                mid_list = tmp
-
-                tmp1 = []
-                for fea in ss[6].split(""):
-                    c = self.catedict[fea] if fea in self.catedict else 0
-                    tmp1.append(c)
-                cat_list = tmp1
-
-                tmp2 = []
-                for fea in ss[7].split(""):
-                    tmp2.append(float(fea))
-                time_list = tmp2
-                
-                #Time-LSTM-123 
-                tmp3 = []
-                for i in range(len(time_list)-1):
-                    deltatime_last = (time_list[i+1] - time_list[i])/(3600 * 24)
-                    if deltatime_last <= 0.5:
-                        deltatime_last = 0.5
-                    tmp3.append(math.log(deltatime_last))
-                deltatime_now = (timestepnow - time_list[-1])/(3600 * 24)
-                if deltatime_now <= 0.5:
-                    deltatime_now = 0.5    
-                tmp3.append(math.log(deltatime_now))               
-                timeinterval_list = tmp3
-
-                #Time-LSTM-4
-                tmp4 = []
-                tmp4.append(0.0)
-                for i in range(len(time_list)-1):
-                    deltatime_last = (time_list[i+1] - time_list[i])/(3600 * 24)
-                    if deltatime_last <= 0.5:
-                        deltatime_last = 0.5
-                    tmp4.append(math.log(deltatime_last))
-                timelast_list = tmp4
-                
-                tmp5 = []
-                for i in range(len(time_list)):
-                    deltatime_now = (timestepnow - time_list[i])/(3600 * 24)
+                    #Time-LSTM-123
+                    tmp3 = []
+                    for i in range(len(time_list)-1):
+                        deltatime_last = (time_list[i+1] - time_list[i])/(3600 * 24)
+                        if deltatime_last <= 0.5:
+                            deltatime_last = 0.5
+                        tmp3.append(math.log(deltatime_last))
+                    deltatime_now = (timestepnow - time_list[-1])/(3600 * 24)
                     if deltatime_now <= 0.5:
                         deltatime_now = 0.5
-                    tmp5.append(math.log(deltatime_now))
-                timenow_list = tmp5
+                    tmp3.append(math.log(deltatime_now))
+                    timeinterval_list = tmp3
 
-                source.append([uid, mid, cat, mid_list, cat_list, timeinterval_list, timelast_list, timenow_list])
-                target.append([float(ss[0]), 1-float(ss[0])])
+                    #Time-LSTM-4
+                    tmp4 = []
+                    tmp4.append(0.0)
+                    for i in range(len(time_list)-1):
+                        deltatime_last = (time_list[i+1] - time_list[i])/(3600 * 24)
+                        if deltatime_last <= 0.5:
+                            deltatime_last = 0.5
+                        tmp4.append(math.log(deltatime_last))
+                    timelast_list = tmp4
 
-                if len(source) >= self.batch_size or len(target) >= self.batch_size:
-                    break
+                    tmp5 = []
+                    for i in range(len(time_list)):
+                        deltatime_now = (timestepnow - time_list[i])/(3600 * 24)
+                        if deltatime_now <= 0.5:
+                            deltatime_now = 0.5
+                        tmp5.append(math.log(deltatime_now))
+                    timenow_list = tmp5
+
+                    source.append([uid, mid, cat, mid_list, cat_list, timeinterval_list, timelast_list, timenow_list])
+                    target.append([float(ss[0]), 1-float(ss[0])])
+
+                    if len(source) >= self.batch_size or len(target) >= self.batch_size:
+                        break
         except IOError:
             self.end_of_data = True
 
         if len(source) == 0 or len(target) == 0:
-            source, target = self.next()
+            source, target = self.__next__()
 
         return source, target
